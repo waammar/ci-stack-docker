3f324e45355d30a920d686193476cdbb1d3742e9
Wael Ammar <wael.ammar@neoxia.com>
2016-05-19 00:46:42 +0200

    add init script

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#Update system
+sudo yum -y update
+
+#Install docker's repo
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+#Install docker engine (last)
+sudo yum install docker-engine
+sudo service docker start
+
+sudo usermod -aG docker centos
+
+sudo su
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
+
+sudo mkdir /srv/docker/jenkins_home
+sudo chown 1000 /srv/docker/jenkins_home/
